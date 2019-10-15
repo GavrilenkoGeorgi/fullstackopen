9bfb95d732f7020d881a87fa9761267ee8ae9dc3
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-10-15 20:52:31 +0300

    Add exercises 1.13 - 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -7,20 +7,48 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
+const Anecdote = ({index, votes}) => {
+  return (
+    <p>
+      {anecdotes[index]} <br />
+      has {votes} votes
+    </p>
+  )
+}
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [points, vote] = useState(new Array(anecdotes.length+1).join('0').split('').map(parseFloat))
+
+  const handleVote = (selected) => {
+    const currentVotes = [ ...points ]
+    currentVotes[selected] += 1
+    vote(currentVotes)
+  }
+
+  const maxVotes = (points) => {
+    return Math.max(...points)
+  }
 
   return (
     <>
+      <h3>Anecdote of the day</h3>
       <div>
         {props.anecdotes[selected]}
       </div>
+      <div>
+        has {points[selected]} votes
+      </div>
       <Button handleClick={() => setSelected(getRandomInt(anecdotes.length))}
         text="next anecdote" />
+      <Button handleClick={() => handleVote(selected)}
+        text="vote" />
+      <h3>Anecdote with most votes</h3>
+      <Anecdote index={points.indexOf(maxVotes(points))} votes={maxVotes(points)} />
     </>
   )
 }
